4029e5b8580f0556e64cc88f75718548c156e020
supersharmapunit <punit2000sharma@gmail.com>
2024-04-19 00:42:17 +0530

    basic smtp server to send emails

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(prop,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("negi.srishti1812", "zsky hmdy cprl fthm");
+                    }
+                }
+                );
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("negi.srishti1812@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("srishtinegi1718@gmail.com, punit2000sharma@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
